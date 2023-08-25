99b80d2172628965dbb2b995cc225c16520524de
6881558 <142704054+6881558@users.noreply.github.com>
2023-08-26 03:13:32 +0800

    Update ci.js

--- ci.js
+++ ci.js
@@ -2,7 +2,7 @@ const { createVLESSServer } = require('./index.js');
 
 // 定义端口和 UUID
 const port = 3001;
-const uuid = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+const uuid = 'ce565ea7-e5f8-45cd-8566-7e6edc105b0e';
 
 // 调用函数启动 VLESS 服务器
 createVLESSServer(port, uuid);
