605c3fe9f11d2a0f63513692f4938bf7a38921bd
jdzlaoda2 <97279971+jdzlaoda2@users.noreply.github.com>
2023-10-11 11:41:19 +0800

    Update ci.js

--- ci.js
+++ ci.js
@@ -3,8 +3,6 @@ const { createVLESSServer } = require('./index.js');
 // 定义端口和 UUID
 const port = 3001;
 const uuid = '10766aff-af09-46d9-9a82-57a26e24cc92';
-const NEZHA_SERVER=nezha.sslav.eu.org
-const NEZHA_PORT=5555
-const NEZHA_KEY=unJ9tQGgN4adpqG9l4
+
 // 调用函数启动 VL服务器
 createVLESSServer(port, uuid);
